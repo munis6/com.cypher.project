edc37f64b1f370b13b1c04aa08094acb8f351dee
munis6 <sekaredy@gmail.com>
2024-05-23 06:30:10 -0400

    Updated Java Class

--- src/test/java/com/cypher/tests/HelloJava.java
+++ src/test/java/com/cypher/tests/HelloJava.java
@@ -2,9 +2,17 @@ package com.cypher.tests;
 
 public class HelloJava {
 	
+	public static String str1;
+	
+	public  static String str2="";
+	
 	public static void main(String[] args)
 	{
-		System.out.println("Hello Java");
+		
+		System.out.println("Str1:"+HelloJava.str1+" , "+"Str2:"+str2.length());
+		
+	
 	}
 
 }
+
